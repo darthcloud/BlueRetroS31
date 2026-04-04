435c4791bb3175b43523c333211c6cef241b9ef5
Jacques Gagnon <darthcloud@gmail.com>
2026-04-04 19:46:57 -0400

    Rename gp01 to wavepak vx

--- main/adapter/wired/wavepak_vx.c
+++ main/adapter/wired/wavepak_vx.c
@@ -4,7 +4,6 @@
  */
 
 #include <string.h>
-#include "driver/i2c_master.h"
 #include "zephyr/types.h"
 #include "tools/util.h"
 #include "adapter/adapter.h"
@@ -13,7 +12,7 @@
 #include "adapter/wireless/wireless.h"
 #include "tests/cmds.h"
 #include "bluetooth/mon.h"
-#include "gp01.h"
+#include "wavepak_vx.h"
 
 #define GP01_AXES_MAX 2
 enum {
@@ -42,44 +41,6 @@ enum {
     GP02_C2R1,
     GP02_C3R1,
 };
-
-static i2c_master_bus_config_t i2c0_config = {
-    .clk_source = I2C_CLK_SRC_DEFAULT,
-    .i2c_port = I2C_NUM_0,
-    .scl_io_num = 33,
-    .sda_io_num = 4,
-    .glitch_ignore_cnt = 7,
-};
-static i2c_master_bus_config_t i2c1_config = {
-    .clk_source = I2C_CLK_SRC_DEFAULT,
-    .i2c_port = I2C_NUM_1,
-    .scl_io_num = 27,
-    .sda_io_num = 15,
-    .glitch_ignore_cnt = 7,
-};
-static i2c_device_config_t wp0_cfg = {
-    .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-    .device_address = 0x40,
-    .scl_speed_hz = 1000000,
-};
-static i2c_device_config_t wp1_cfg = {
-    .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-    .device_address = 0x48,
-    .scl_speed_hz = 1000000,
-};
-static i2c_device_config_t wp2_cfg = {
-    .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-    .device_address = 0x50,
-    .scl_speed_hz = 1000000,
-};
-static i2c_master_bus_handle_t i2c0_handle;
-static i2c_master_bus_handle_t i2c1_handle;
-static i2c_master_dev_handle_t wp00_handle;
-static i2c_master_dev_handle_t wp01_handle;
-static i2c_master_dev_handle_t wp02_handle;
-static i2c_master_dev_handle_t wp10_handle;
-static i2c_master_dev_handle_t wp11_handle;
-static i2c_master_dev_handle_t wp12_handle;
 
 static DRAM_ATTR const uint8_t gp01_axes_idx[ADAPTER_MAX_AXES] =
 {
@@ -116,21 +77,6 @@ static DRAM_ATTR const uint32_t gp01_btns_mask[32] = {
     0, 0, 0, 0,
     0, 0, 0, 0,
 };
-
-void gp01_init(void) {
-    i2c_new_master_bus(&i2c0_config, &i2c0_handle);
-    i2c_master_bus_add_device(i2c0_handle, &wp0_cfg, &wp00_handle);
-    i2c_master_bus_add_device(i2c0_handle, &wp1_cfg, &wp01_handle);
-    i2c_master_bus_add_device(i2c0_handle, &wp2_cfg, &wp02_handle);
-    uint8_t pot_test[] = {0x91, 0x00, 0x80};
-    uint8_t oamp_init[] = {0x3E, 0xC0};
-    i2c_master_transmit(wp00_handle, (uint8_t *)pot_test, sizeof(pot_test), -1);
-    i2c_master_transmit(wp00_handle, (uint8_t *)oamp_init, sizeof(oamp_init), -1);
-    i2c_master_transmit(wp01_handle, (uint8_t *)pot_test, sizeof(pot_test), -1);
-    i2c_master_transmit(wp01_handle, (uint8_t *)oamp_init, sizeof(oamp_init), -1);
-    i2c_master_transmit(wp02_handle, (uint8_t *)pot_test, sizeof(pot_test), -1);
-    printf("%s: I2C init done\n", __FUNCTION__);
-}
 
 void IRAM_ATTR gp01_init_buffer(int32_t dev_mode, struct wired_data *wired_data) {
     struct gp01_map *map = (struct gp01_map *)wired_data->output;
